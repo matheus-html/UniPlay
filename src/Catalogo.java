a0a0f73ff89436ab0d5beac3660c6bb75e22a98b
matheus <matheus.holanda101@gmail.com>
2025-04-06 14:23:18 -0300

    Remoção de CatalogoJogos

--- src/Catalogo.java
+++ src/Catalogo.java
@@ -1,26 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Catalogo {
-    private CatalogoJogos catalogo;
+    private List<Jogo> jogos = new ArrayList<>();
 
     public Catalogo() {
-        catalogo = new CatalogoJogos();
 
-        catalogo.adicionarJogo(new JogoDigital("Cyberpunk 2077", "RPG", 199.90, 70.5));
-        catalogo.adicionarJogo(new JogoFisico("FIFA 23", "Esporte", 249.90, 15));
-        catalogo.adicionarJogo(new JogoDigital("The Witcher 3","RPG",  79.90, 45.0));
+        jogos.add(new JogoDigital("Cyberpunk 2077", "RPG", 199.90, 70.5));
+        jogos.add(new JogoFisico("Assassin's Creed Shadows", "RPG", 249.90, 15));
+        jogos.add(new JogoDigital("The Witcher 3","RPG",  79.90, 45.0));
+        jogos.add(new JogoDigital("Animal Crossing","Simulator",  399.90, 7.0));
     }
 
     public List<Jogo> getJogos() {
-        return catalogo.getTodosJogos();
+        return new ArrayList<>(jogos);
     }
 
     public Jogo jogoIndex(int index) {
-        List<Jogo> jogos = catalogo.getTodosJogos();
-
         if (index >= 0 && index < jogos.size()) {
             return jogos.get(index);
-        } else{
+        } else {
             return null;
         }
     }
